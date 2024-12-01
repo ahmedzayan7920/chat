0c37da07cd03e74d9f018fa55b0718895407e5eb
Ahmed Zayan <ahmedabdallazayan@gmail.com>
2024-12-01 02:27:59 +0200

    Add UsersCubit and UsersState for managing user data
    Introduce `UsersCubit` and associated `UsersState` classes to handle user data fetching and state management using the BLoC pattern.
    
    The `UsersCubit` interacts with `UsersRepository` to fetch user data and emits different states (`UsersLoadingState`, `UsersLoadedState`, `UsersErrorState`) based on the operation's outcome.

---
+++ lib/features/users/logic/users_cubit.dart
@@ -0,0 +1,21 @@
+import 'package:chat/features/users/repos/users_repository.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'users_state.dart';
+
+class UsersCubit extends Cubit<UsersState> {
+  final UsersRepository _usersRepository;
+
+  UsersCubit({required UsersRepository usersRepository})
+      : _usersRepository = usersRepository,
+        super(const UsersInitialState());
+
+  Future<void> fetchAllUsers() async {
+    emit(const UsersLoadingState());
+    final result = await _usersRepository.fetchAllUsers();
+    result.fold(
+      (failure) => emit(UsersErrorState(failure.message)),
+      (users) => emit(UsersLoadedState(users)),
+    );
+  }
+}
