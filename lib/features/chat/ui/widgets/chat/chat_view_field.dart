8313e6f722e50f0fc327fb9d991071e60841f899
Ahmed Zayan <ahmedabdallazayan@gmail.com>
2024-12-06 18:46:31 +0200

    Refactor chat view field to use TextFormField with enhanced UI
    Updated the chat view field to use `TextFormField` instead of `CustomTextFormField` for better customization and control.
    
    Added a media icon button and improved the overall styling of the input field, including borders and label styles.
    
    Moved the send message logic to a separate method for better readability and maintainability.

--- lib/features/chat/ui/widgets/chat/chat_view_field.dart
+++ lib/features/chat/ui/widgets/chat/chat_view_field.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../../../core/utils/app_strings.dart';
-import '../../../../../core/widgets/custom_text_form_field.dart';
 import '../../../../auth/logic/auth_cubit.dart';
 import '../../../../auth/logic/auth_state.dart';
 import '../../../logic/chat/chat_cubit.dart';
@@ -31,32 +30,75 @@ class _ChatViewFieldState extends State<ChatViewField> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: CustomTextFormField(
-        textInputAction: TextInputAction.done,
-        keyboardType: TextInputType.text,
-        label: AppStrings.sendMessageHint,
-        prefixIcon: Icons.message_outlined,
+      padding: const EdgeInsets.all(8),
+      child: TextFormField(
         controller: _messageController,
-        suffixIcon: Icons.send_outlined,
-        suffixOnPressed: () {
-          if (_messageController.text.trim().isNotEmpty) {
-            final currentUserId =
-                (context.read<AuthCubit>().state as AuthenticatedState).user.id;
-            final otherUserId = widget.chat.members.firstWhere(
-              (element) => element != currentUserId,
-              orElse: () {
-                return currentUserId;
-              },
-            );
-            context.read<ChatCubit>().sendTextMessage(
-                  currentUserId: currentUserId,
-                  otherUserId: otherUserId,
-                  message: _messageController.text.trim(),
-                );
-            _messageController.clear();
-          }
+        decoration: InputDecoration(
+          label: Text(
+            AppStrings.sendMessageHint,
+            style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+          ),
+          labelStyle: TextStyle(
+            color: Colors.grey.shade700,
+          ),
+          suffixIcon: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.perm_media_outlined),
+                color: Theme.of(context).colorScheme.onSecondary,
+              ),
+              IconButton(
+                onPressed: _sendTextMessage,
+                icon: Icon(Icons.send_outlined),
+                color: Theme.of(context).colorScheme.onSecondary,
+              ),
+            ],
+          ),
+          filled: true,
+          fillColor: Theme.of(context).colorScheme.secondary,
+          border: _buildBorder(color: Colors.grey.shade400),
+          disabledBorder: _buildBorder(color: Colors.grey.shade300),
+          enabledBorder:
+              _buildBorder(color: Theme.of(context).colorScheme.tertiary),
+          focusedBorder:
+              _buildBorder(color: Theme.of(context).colorScheme.primary),
+          errorBorder: _buildBorder(color: Colors.red),
+          focusedErrorBorder: _buildBorder(color: Colors.red),
+        ),
+      ),
+    );
+  }
+
+  _sendTextMessage() {
+    if (_messageController.text.trim().isNotEmpty) {
+      final currentUserId =
+          (context.read<AuthCubit>().state as AuthenticatedState).user.id;
+      final otherUserId = widget.chat.members.firstWhere(
+        (element) => element != currentUserId,
+        orElse: () {
+          return currentUserId;
         },
+      );
+      context.read<ChatCubit>().sendTextMessage(
+            currentUserId: currentUserId,
+            otherUserId: otherUserId,
+            message: _messageController.text.trim(),
+          );
+      _messageController.clear();
+    }
+  }
+
+  OutlineInputBorder _buildBorder({required Color color}) {
+    return OutlineInputBorder(
+      borderRadius: BorderRadius.circular(8),
+      borderSide: BorderSide(
+        color: color,
+        width: 2,
+        style: BorderStyle.solid,
       ),
     );
   }
