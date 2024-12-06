9bf5c8cb27c74094862de1a9edcac577307cc68b
Ahmed Zayan <ahmedabdallazayan@gmail.com>
2024-12-06 14:34:48 +0200

    Add Supabase initialization and configuration
    Update .gitignore to include supabase_config.dart.
    
    Modify main.dart to import Supabase and initialize it using the configuration from supabase_config.dart.

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,20 @@
 import 'package:chat/chat_app.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'core/di/dependency_injection.dart';
+import 'core/supabase/supabase_config.dart';
 import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await Supabase.initialize(
+    url: SupabaseConfig.supabaseUrl,
+    anonKey: SupabaseConfig.supabaseKey,
   );
   setupDependencyInjection();
   runApp(const ChatApp());
