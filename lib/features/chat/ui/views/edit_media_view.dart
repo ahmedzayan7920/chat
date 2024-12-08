d81d63f41e904114066fa55fff45592c44876476
Ahmed Zayan <ahmedabdallazayan@gmail.com>
2024-12-08 21:24:33 +0200

    Add EditMediaArgumentsModel, CustomVideoPlayer, and EditMediaView
    Introduce `EditMediaArgumentsModel` for passing arguments to media editing views.
    
    Add `CustomVideoPlayer` for playing local and network videos with controls.
    
    Implement `EditMediaView` for editing media with a caption input and send functionality.
    
    Update `pubspec.yaml` and `pubspec.lock` to include necessary dependencies.

---
+++ lib/features/chat/ui/views/edit_media_view.dart
@@ -0,0 +1,85 @@
+import 'dart:io';
+
+import 'package:chat/core/utils/app_strings.dart';
+import 'package:chat/core/widgets/custom_text_form_field.dart';
+import 'package:chat/core/widgets/spaces.dart';
+import 'package:flutter/material.dart';
+
+import '../../../../core/widgets/custom_video_player.dart';
+
+class EditMediaView extends StatefulWidget {
+  final String mediaPath;
+  final bool isVideo;
+  final Function(String caption) onSend;
+
+  const EditMediaView({
+    super.key,
+    required this.mediaPath,
+    required this.isVideo,
+    required this.onSend,
+  });
+
+  @override
+  State<EditMediaView> createState() => _EditMediaViewState();
+}
+
+class _EditMediaViewState extends State<EditMediaView> {
+  final TextEditingController _captionController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  dispose() {
+    _captionController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(AppStrings.editMedia),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: widget.isVideo
+                ? CustomVideoPlayer(
+                    videoPath: widget.mediaPath,
+                    isLocal: true,
+                  )
+                : Image.file(
+                    File(widget.mediaPath),
+                    fit: BoxFit.contain,
+                  ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: CustomTextFormField(
+                    controller: _captionController,
+                    label: AppStrings.addCaption,
+                    prefixIcon: Icons.text_fields_outlined,
+                  ),
+                ),
+                HorizontalSpace(width: 8),
+                IconButton(
+                  icon: const Icon(Icons.send),
+                  onPressed: () {
+                    widget.onSend(_captionController.text.trim());
+                    Navigator.pop(context);
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
