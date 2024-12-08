cd7853c1e60bc8c51581a0b51287e4171e25b0b1
Ahmed Zayan <ahmedabdallazayan@gmail.com>
2024-12-08 21:27:02 +0200

    Add routes and views for media editing and video playback
    Added new routes and corresponding views for editing media and playing videos.
    
    This includes `EditMediaView` and `VideoPlayerView`, along with their respective routes in `AppRoutes`.

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -2,12 +2,15 @@ import 'package:chat/features/auth/ui/views/phone_auth_view.dart';
 import 'package:chat/features/auth/ui/views/phone_otp_view.dart';
 import 'package:chat/features/chat/models/chat_model.dart';
 import 'package:chat/features/chat/ui/views/chat_view.dart';
+import 'package:chat/features/chat/ui/views/edit_media_view.dart';
 import 'package:flutter/material.dart';
 
 import '../../features/auth/ui/views/login_view.dart';
 import '../../features/auth/ui/views/register_view.dart';
 import '../../features/chat/ui/views/chats_view.dart';
+import '../../features/chat/ui/views/video_player_view.dart';
 import '../../features/users/ui/views/users_view.dart';
+import '../models/edit_media_arguments_model.dart';
 import 'app_routes.dart';
 
 class AppRouter {
@@ -30,6 +33,23 @@ class AppRouter {
       case AppRoutes.chat:
         final chat = settings.arguments as ChatModel;
         return MaterialPageRoute(builder: (_) => ChatView(chat: chat));
+      case AppRoutes.editMedia:
+        final args = settings.arguments as EditMediaArgumentsModel;
+        return MaterialPageRoute(
+          builder: (_) => EditMediaView(
+            mediaPath: args.mediaPath,
+            isVideo: args.isVideo,
+            onSend: args.onSend,
+          ),
+        );
+
+      case AppRoutes.videoPlayer:
+        final videoUrl = settings.arguments as String;
+        return MaterialPageRoute(
+          builder: (_) => VideoPlayerView(
+            videoUrl: videoUrl,
+          ),
+        );
       default:
         return MaterialPageRoute(
           builder: (_) => Scaffold(
