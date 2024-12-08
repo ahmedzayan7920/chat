cbb08780b13effa49308584d2459c5f7e9a85727
Ahmed Zayan <ahmedabdallazayan@gmail.com>
2024-12-08 21:14:35 +0200

    Add extensions for AuthCubit, ChatModel, and Duration
    Added extensions to `AuthCubit` to get the current user ID, to `ChatModel` to get the other user ID in a chat, and to `Duration` to format the duration as a string.
    
    Also, updated the import statements to include necessary files.

--- lib/core/extensions/extensions.dart
+++ lib/core/extensions/extensions.dart
@@ -1,5 +1,9 @@
 import 'package:intl/intl.dart';
 
+import '../../features/auth/logic/auth_cubit.dart';
+import '../../features/auth/logic/auth_state.dart';
+import '../../features/chat/models/chat_model.dart';
+
 extension StringsListExtension on List<String> {
   String generateChatId() {
     final userId1 = this[0];
@@ -13,5 +17,23 @@ extension TimeExtension on int {
   String formatIntTime() {
     final DateTime time = DateTime.fromMillisecondsSinceEpoch(this);
     return DateFormat('hh:mm a').format(time);
+  }
+}
+
+extension AuthCubitExtensions on AuthCubit {
+  String get currentUserId => (state as AuthenticatedState).user.id;
+}
+
+extension ChatModelExtensions on ChatModel {
+  String getOtherUserId(String currentUserId) {
+    return members.firstWhere((id) => id != currentUserId);
+  }
+}
+
+extension DurationExtensions on Duration {
+  String formatDuration() {
+    final minutes = inMinutes.toString().padLeft(2, '0');
+    final seconds = (inSeconds % 60).toString().padLeft(2, '0');
+    return '$minutes:$seconds';
   }
 }
