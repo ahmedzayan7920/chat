8a8bb362127e75ee12417a7b30467947fe6147dd
Ahmed Zayan <ahmedabdallazayan@gmail.com>
2024-12-11 17:42:37 +0200

    Add name parameter to registerWithEmailAndPassword and handle additional exceptions
    - Added `name` parameter to `registerWithEmailAndPassword` method in `AuthDataSource` and its implementation in `FirebaseAuthDataSource`.
    - Handle `PathNotFoundException`, `PostgrestException`, and `StorageException` in the `Failure` class.
    - Added checks for `null` user in `registerWithEmailAndPassword` and updated user display name.
    - Used `AppStrings` for error messages in `FirebaseAuthDataSource`.

--- lib/core/models/failure.dart
+++ lib/core/models/failure.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class Failure {
   final String message;
@@ -13,6 +14,12 @@ class Failure {
     } else if (e is FirebaseException) {
       return Failure(e.message ?? e.toString());
     } else if (e is SocketException) {
+      return Failure(e.message);
+    } else if (e is PathNotFoundException) {
+      return Failure(e.message);
+    } else if (e is PostgrestException) {
+      return Failure(e.message);
+    } else if (e is StorageException) {
       return Failure(e.message);
     } else {
       return Failure(e.toString());
