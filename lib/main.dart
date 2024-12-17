db088675ad5776cb77b00b9ac123037605dababf
Ahmed Zayan <ahmedabdallazayan@gmail.com>
2024-12-18 01:07:31 +0200

    Add Firebase Crashlytics and Analytics
    Integrate Firebase Crashlytics for error reporting and Firebase Analytics for tracking user interactions in the ChatApp.
    
    Update `build.gradle` and `settings.gradle` to include the necessary plugins, and modify `main.dart` and `chat_app.dart` to configure and use these services.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
@@ -16,6 +18,15 @@ void main() async {
     url: SupabaseConfig.supabaseUrl,
     anonKey: SupabaseConfig.supabaseKey,
   );
+  if (!kDebugMode) {
+    FlutterError.onError = (errorDetails) {
+      FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+    };
+    PlatformDispatcher.instance.onError = (error, stack) {
+      FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+      return true;
+    };
+  }
   setupDependencyInjection();
   runApp(const ChatApp());
 }
